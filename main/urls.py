28721d248ed67fd1c4a60fde687b717f4027a948
Varshana01 <varshnidhi19@gmail.com>
2022-01-14 01:35:19 +0400

    final

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views # . is shorthand for the current directory
 
 
 urlpatterns=[
-    path('',views.index, name='home'),
+    path('\index',views.index, name='home'),
     # path('about/', views.about, name='about')
 ]
